4565723302a90f66949be6e68e6550eb2815800b
Mohammad Fahad <fahadmo0hammad@gmail.com>
2021-12-12 11:49:56 +0600

    WPF project added

---
+++ HotelApp.Desktop/App.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Databases;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static ServiceProvider serviceProvider;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+            services.AddTransient<MainWindow>();
+            services.AddTransient<CheckInForm>();
+            services.AddTransient<ISqlDataAccess, SqlDataAccess>();
+            services.AddTransient<ISqLiteDataAccess, SqLiteDataAccess>();
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfiguration config = builder.Build();
+
+            services.AddSingleton(config);
+
+            var dbChoice = config.GetValue<string>("DatabaseChoice").ToLower();
+
+            if (dbChoice == "sql")
+            {
+                services.AddTransient<IDatabaseData, SqlData>();
+            }
+            else if (dbChoice == "sqlite")
+            {
+                services.AddTransient<IDatabaseData, SqLiteData>();
+            }
+            else
+            {
+                //Default
+                services.AddTransient<IDatabaseData, SqlData>();
+            }
+
+            serviceProvider = services.BuildServiceProvider();
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+
+            mainWindow.Show();
+        }    
+    }
+}
---
+++ HotelApp.Desktop/CheckInForm.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
+
+namespace HotelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for CheckInForm.xaml
+    /// </summary>
+    public partial class CheckInForm : Window
+    {
+        private readonly IDatabaseData _db;
+        private BookingFullModel _data;
+
+        public CheckInForm(IDatabaseData db)
+        {
+            _db = db;
+            InitializeComponent();
+        }
+
+        public void PopulateCheckInInfo(BookingFullModel data)
+        {
+            _data = data;
+            firstNameText.Text = _data.FirstName;
+            lastNameText.Text = _data.LastName;
+            titleText.Text = _data.Title;
+            roomNumberText.Text = _data.RoomNumber;
+            totalCostText.Text = $"{_data.TotalCost:C}";
+        }
+
+        private void checkInUser_Click(object sender, RoutedEventArgs e)
+        {
+            _db.CheckIn(_data.Id);
+            this.Close();
+        }
+    }
+}
