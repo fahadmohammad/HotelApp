b036ca37162b494c4e8f0ee533f03e853dd7cdcf
Mohammad Fahad <fahadmo0hammad@gmail.com>
2021-12-12 11:47:05 +0600

    Sql db project added along with post deployment script and store procedures.

---
+++ HotelAppDB/dbo/StoredProcedures/spBookings_Insert.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spBookings_Insert]
+	@roomId int,
+	@guestId int,
+	@startDate date,
+	@endDate date,
+	@totalcost money
+AS
+begin
+	set nocount on;
+
+	insert into dbo.Bookings(RoomId, GuestId, StartDate, EndDate, TotalCost)
+	values (@roomId, @guestId, @startDate, @endDate, @totalcost);
+	
+end
---
+++ HotelAppDB/dbo/StoredProcedures/spGuests_Insert.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[spGuests_Insert]
+	@firstName nvarchar(50),
+	@lastName nvarchar(50)
+AS
+begin
+	set nocount on;
+
+	if not exists (select 1 from dbo.Guests where FirstName = @firstName and LastName = @lastName)
+	begin
+		insert into dbo.Guests (FirstName, LastName) values (@firstName, @lastName);
+	end
+
+	select top 1 g.Id, g.FirstName, g.LastName 
+	from dbo.Guests g where FirstName = @firstName and LastName = @lastName
+end
